fc56d0f7b12387a2e617ae4916ec4901d4095b7f
Miguel Galarreta Valverde <miguelg@ime.usp.br>
2013-03-14 22:22:51 -0300

    added another test

--- public_html/tests.js
+++ public_html/tests.js
@@ -5,6 +5,8 @@ test( "test true", function() {
 ok( 1 == "1", "Passed!" );
 });
 
-test( "values loaded", function(){
-    
+test( "load file", function(){
+    var file='http://x.babymri.org/?vol.nrrd'
+    loadWebFile(file); 
+    ok( file==sessionStorage.getItem("volumePath") ,"Passed!");
 });
