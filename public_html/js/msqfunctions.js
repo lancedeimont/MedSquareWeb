6e7451e909d1f2e7adcf20f82686b33aededb0d5
Miguel Angel Galarreta V <miguelg@ime.usp.br>
2013-03-22 15:26:45 -0300

    improving tests part 1+

--- public_html/js/msqfunctions.js
+++ public_html/js/msqfunctions.js
@@ -2,5 +2,14 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
+function checkLength(o, n, min, max) {
+    if (o.val().length > max || o.val().length < min) {
+        o.addClass("ui-state-error");
+        updateTips("Length of " + n + " must be between " +
+                min + " and " + max + ".");
+        return false;
+    } else {
+        return true;
+    }
+}
 
--- public_html/tests/tests.js
+++ public_html/tests/tests.js
@@ -5,6 +5,11 @@ test( "test true", function() {
 ok( 1 == "1", "Passed!" );
 });
 
+test("checkLength",function(){
+    var rows =$( "#rows" )
+    ok(checkLength( rows, "rows", 1, 3 ));
+});
+
 test( "load file", function(){
     var file='http://x.babymri.org/?vol.nrrd'
     loadWebFile(file); 
