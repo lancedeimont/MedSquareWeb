e9a8c4413daa247a9b2cb44aa5b0796bd958dfd5
Miguel Angel Galarreta V <miguelg@ime.usp.br>
2013-03-27 20:45:44 -0300

    improved publish script

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd public_html;
-rsync -a --exclude=data miguelgalarreta@vision.ime.usp.br:/home/miguelgalarreta/www/medsquareweb/
+rsync -e 'ssh -ax' -av --exclude=data miguelgalarreta@vision.ime.usp.br:/home/miguelgalarreta/www/medsquareweb/
